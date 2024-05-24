4b04ebdf1db3b585fb92e73d11b750737ffb3b13
SaiKrishna <saikrishna.gorantla@sportsplus.email>
2024-05-24 18:59:38 +0530

    first

--- main-spring-app/src/main/java/com/example/demo/home/HomeController.java
+++ main-spring-app/src/main/java/com/example/demo/home/HomeController.java
@@ -1,5 +1,8 @@
 package com.example.demo.home;
 
-public class HomeController {
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+public class HomeController {
+	
 }
