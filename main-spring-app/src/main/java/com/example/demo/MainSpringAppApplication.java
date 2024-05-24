4e5c592290967d25d469a2901d86b0885e8aadfc
SaiKrishna <saikrishna.gorantla@sportsplus.email>
2024-05-24 18:33:29 +0530

    Welcome commit from spring boot.

--- main-spring-app/src/main/java/com/example/demo/MainSpringAppApplication.java
+++ main-spring-app/src/main/java/com/example/demo/MainSpringAppApplication.java
@@ -8,6 +8,7 @@ public class MainSpringAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MainSpringAppApplication.class, args);
+		System.out.println("Welcome");
 	}
 
 }
