9f561fd665d131ec6cefd3f942ee050e281bf072
SaiKrishna <saikrishna.gorantla@sportsplus.email>
2024-05-24 19:02:39 +0530

    second

--- main-spring-app/src/main/java/com/example/demo/home/HomeController.java
+++ main-spring-app/src/main/java/com/example/demo/home/HomeController.java
@@ -1,8 +1,13 @@
 package com.example.demo.home;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 	
+	@RequestMapping("/")
+	public String homeController() {
+		return "Welcome Home";
+	}
 }
